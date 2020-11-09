c1477aaa05fff73c8dd5b50552f383222cdfad58
quiles004 <73704174+quiles004@users.noreply.github.com>
2020-11-09 13:21:01 +0100

    Add files via upload

---
+++ proyecto2.js
@@ -0,0 +1,10 @@
+use("proyecto2")
+
+db.inventory.insertMany([
+   { item: "agenda", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" },
+   { item: "cuaderno", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "A" },
+   { item: "folios", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" },
+   { item: "libro", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
+   { item: "carta", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" }
+]);
+db.inventory.find({})
